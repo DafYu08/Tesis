55d0120dbe5a682513da8b91dfc95c7016898144
Dafne Sol Yudcovsky <dafneyudcovsky@gmail.com>
2025-04-26 15:25:24 -0300

    26/4 15.25
    Terminada la restricción 5

--- script_correr_solver.sh
+++ script_correr_solver.sh
@@ -1,4 +1,8 @@
-for tasa in 37.02 30 25 20; do
+for i in $(seq 6 25); do
+    python src/Restricciones_Deseables/Modelo_restricciones_deseables.py $i 5 30
+done
+
+for tasa in 25 20; do
     for i in $(seq 1 25); do
         python src/Restricciones_Deseables/Modelo_restricciones_deseables.py $i 5 $tasa
     done
