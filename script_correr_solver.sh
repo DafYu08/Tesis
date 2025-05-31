69e2edf8bb5fa11512f9d8520c0ff0ba21e3d96f
Dafne Sol Yudcovsky <dafneyudcovsky@gmail.com>
2025-05-31 19:09:47 -0300

    31/5 19.09

--- script_correr_solver.sh
+++ script_correr_solver.sh
@@ -1,5 +1,11 @@
-for tasa in 37.02 30 25 20; do
-    for i in $(seq 1 25); do
+for tasa in 37.02; do
+    for i in $(seq 4 25); do
+        python src/Restricciones_Deseables/Modelo_restricciones_deseables.py $i 2 $tasa
+    done
+done
+
+for tasa in 30; do
+    for i in $(seq 1 10); do
         python src/Restricciones_Deseables/Modelo_restricciones_deseables.py $i 2 $tasa
     done
 done
